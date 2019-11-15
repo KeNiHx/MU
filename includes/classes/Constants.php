5f087b8f23e9b6abbdd069cfe94546cefde5a11f
KeNiHx <klam291@mtroyal.ca>
2019-11-14 21:05:16 -0700

    connected to db and posted data to db

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -1,10 +1,13 @@
 <?php
 class Constants {
+
         public static $userNameCharacters = "Your username must be between 5 and 25 characters.";
+        public static $usernameTaken = "This username already exists";
         public static $firstNameCharacters = "Your first name  must be between 2 and 25 characters.";
         public static $lastNameCharacters = "Your last name must be between 5 and 25 characters.";
         public static $emailsDoNotMatch = "Your emails don't match!";
         public static $emailInvalid = "Email is invalid!";
+        public static $emailTaken = "This email is already in use";
         public static $passwordsDoNotMatch = "Your passwords don't match!";
         public static $passwordNotAlphanumeric = "Your passwords can only contain numbers and letters.";
         public static $passwordCharacters = "Your password must be between 5 and 30 characters.";
--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -34,17 +34,18 @@ if(isset($_POST['registerButton'])){
 
     //Use function instead to clean code duplication
     $username = sanitizeFormUsername($_POST['username']);
-    $firstName = sanitizeFormString($_POST['firstName']);
-    $lastName = sanitizeFormString($_POST['lastName']);    
-    $email = sanitizeFormString($_POST['email']);
-    $email2 = sanitizeFormString($_POST['email2']);
-    $password = sanitizeFormPassword($_POST['password']);
-    $password2 = sanitizeFormPassword($_POST['password2']);
+	$firstName = sanitizeFormString($_POST['firstName']);
+	$lastName = sanitizeFormString($_POST['lastName']);
+	$email = sanitizeFormString($_POST['email']);
+	$email2 = sanitizeFormString($_POST['email2']);
+	$password = sanitizeFormPassword($_POST['password']);
+	$password2 = sanitizeFormPassword($_POST['password2']);
 
     $wasSuccessful = $account->register($username, $firstName, $lastName, $email, $email2, $password, $password2);
-    if($wasSuccessful == true){
-        header("Location: index.php");
-    }
+
+	if($wasSuccessful == true) {
+		header("Location: index.php");
+	}
 }
 
 ?>

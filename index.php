535c277e49480e9e1d643f7dd02006108ba3f3ba
KeNiHx <klam291@mtroyal.ca>
2019-11-16 10:40:43 -0700

    added progress bar and playback bar

--- index.php
+++ index.php
@@ -51,6 +51,14 @@ if(isset($_SESSION['userLoggedIn'])){
                 
                     </div>
 
+                    <div class="playbackBar">
+
+                        <span class="progressTime current">0.00</span>
+                        <div class="progressBar"></div>
+                        <span class="progressTime remaining">0.00</span>
+
+                    </div>
+
                 </div>
             </div>
             <div id="nowPlayingRight">

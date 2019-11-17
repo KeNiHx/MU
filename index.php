70b2ded8cbfe3529ec165e9148839170661b9289
KeNiHx <klam291@mtroyal.ca>
2019-11-17 10:18:31 -0700

    added logo

--- index.php
+++ index.php
@@ -19,7 +19,11 @@ if(isset($_SESSION['userLoggedIn'])){
     <div id="mainContainer"> 
         <div id="topContainer">
             <div id="navBarContainer">
-                   
+                <nav class="navBar">
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/icons/logo.png">
+                    </a>
+                </nav>   
             </div>
         </div>
         <div id="nowPlayingBarContainer">

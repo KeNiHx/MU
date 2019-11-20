3858e985495e47bbd4e4fe11f055e1bc277ad171
KeNiHx <klam291@mtroyal.ca>
2019-11-19 22:52:51 -0700

    added header and footer files

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
-<?php
-include("includes/config.php");
-
-// session_destroy(); LOGOUT
-
-if(isset($_SESSION['userLoggedIn'])){
-    $userLoggedIn = $_SESSION['userLoggedIn'];
-}else{
-    header("Location: register.php");
-}
-?>
-
-<html>
-<head>
-    <title>Welcome to MU's!</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
-<body>
-    <div id="mainContainer"> 
-        <div id="topContainer">
-            <?php include("includes/navBarContainer.php"); ?>
-            <div id="mainViewContainer">
-                <div id="mainContent">
+<?php include("includes/header.php"); ?>
                 
-                </div>
-            </div>
-        </div>
-
-        <?php include("includes/nowPlayingBar.php"); ?>
-
-
-    </div>
-</body>
-</html>
+<?php include("includes/footer.php"); ?>

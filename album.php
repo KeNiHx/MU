6b454a5da6a020debfe0e974968a217b6a60037a
KeNiHx <klam291@mtroyal.ca>
2019-11-22 22:12:33 -0700

    display number of songs

--- album.php
+++ album.php
@@ -8,12 +8,27 @@ if(isset($_GET['id'])) {
 
 
 $album = new Album($con,$albumId);
-
 $artist = $album-> getArtist();
-
-echo $album->getTitle() . "<br>";
-echo  $artist->getName();
 ?>
+
+
+<div class="entityInfo">
+
+    <div class="leftSection">
+        <img src="<?php echo $album->getArtworkPath(); ?>">
+    </div>
+
+    <div class="rightSection">
+        <h2><?php echo $album->getTitle(); ?></h2>
+        <span>By <?php echo $artist->getName(); ?></span>
+        <p><?php echo $album->getNumberOfSongs(); ?> Songs</p>
+    </div>
+
+
+</div>
+
+
+
 
 
 

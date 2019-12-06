cd839e819d7f24790ea92b0257bc9a848a93168a
KeNiHx <klam291@mtroyal.ca>
2019-12-05 21:49:46 -0700

    updating play count, time remaining

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,37 @@
 var currentPlaylist = [];
 var audioElement;
 
+function formatTime(seconds) {
+    var time = Math.round(seconds);
+    var minutes = Math.floor(time / 60); // rounds the number down
+    var seconds = time - (minutes * 60);
+
+    var extraZero = (seconds < 10) ? "0" :  "";
+    
+    return minutes + ":" + extraZero + seconds;
+}
 
 function Audio() {
 
-    this.currentlyPlaying;
-    this.audio = document.createElement('audio');
+	this.currentlyPlaying;
+	this.audio = document.createElement('audio');
+    this.audio.addEventListener("canplay", function() {
+        //this refers to the object that the even was called on
+        var duration = formatTime(this.duration);
+        $(".progressTime.remaining").text(duration);
+    });
+    
+	this.setTrack = function(track) {
+		this.currentlyPlaying = track;
+		this.audio.src = track.path;
+	}
 
-    this.setTrack = function(src) {
-        this.audio.src = src;
-    }
+	this.play = function() {
+		this.audio.play();
+	}
 
-    this.play = function() {
-        this.audio.play();
-    }
+	this.pause = function() {
+		this.audio.pause();
+	}
 
-    this.pause = function() {
-        this.audio.pause();
-    }
 }

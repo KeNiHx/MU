1bc5e1ac05c641e0c76668be21b22336473f172c
KeNiHx <klam291@mtroyal.ca>
2019-12-06 19:56:43 -0700

    added shuffle functionality

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -13,43 +13,52 @@ $jsonArray = json_encode($resultArray);
 <script>
 
 $(document).ready(function() {
-	currentPlaylist = <?php echo $jsonArray; ?>;
+	var newPlaylist = <?php echo $jsonArray; ?>;
 	audioElement = new Audio();
-	setTrack(currentPlaylist[0], currentPlaylist, false);
+	setTrack(newPlaylist[0], newPlaylist, false);
 	updateVolumeProgressBar(audioElement.audio);
+
 
 	$("#nowPlayingBarContainer").on("mousedown touchstart mousemove touchmove", function(e) {
 		e.preventDefault();
-	})
+	});
 
-	$(".playbackBar .progressBar").mousedown(function(){
+
+	$(".playbackBar .progressBar").mousedown(function() {
 		mouseDown = true;
-	})
-	$(".playbackBar .progressBar").mousemove(function(e){
-		if(mouseDown == true){
-			//set time of song, depending on position of mouse
+	});
+
+	$(".playbackBar .progressBar").mousemove(function(e) {
+		if(mouseDown == true) {
+			//Set time of song, depending on position of mouse
 			timeFromOffset(e, this);
 		}
 	});
-	$(".playbackBar .progressBar").mouseup(function(e){
+
+	$(".playbackBar .progressBar").mouseup(function(e) {
 		timeFromOffset(e, this);
 	});
 
-	$(".volumeBar .progressBar").mousedown(function(){
+
+	$(".volumeBar .progressBar").mousedown(function() {
 		mouseDown = true;
-	})
-	$(".volumeBar .progressBar").mousemove(function(e){
-		if(mouseDown == true){
+	});
+
+	$(".volumeBar .progressBar").mousemove(function(e) {
+		if(mouseDown == true) {
+
 			var percentage = e.offsetX / $(this).width();
-			if(percentage >= 0 && percentage <=1){
+
+			if(percentage >= 0 && percentage <= 1) {
 				audioElement.audio.volume = percentage;
 			}
-			
 		}
 	});
-	$(".volumeBar .progressBar").mouseup(function(e){
+
+	$(".volumeBar .progressBar").mouseup(function(e) {
 		var percentage = e.offsetX / $(this).width();
-		if(percentage >= 0 && percentage <=1){
+
+		if(percentage >= 0 && percentage <= 1) {
 			audioElement.audio.volume = percentage;
 		}
 	});
@@ -57,19 +66,23 @@ $(document).ready(function() {
 	$(document).mouseup(function() {
 		mouseDown = false;
 	});
+
+
+
+
 });
 
-function timeFromOffset(mouse, progressBar){
-		var percentage = mouse.offsetX / $(progressBar).width() * 100;
-		var seconds = audioElement.audio.duration * (percentage / 100);
-		audioElement.setTime(seconds);
-	}
+function timeFromOffset(mouse, progressBar) {
+	var percentage = mouse.offsetX / $(progressBar).width() * 100;
+	var seconds = audioElement.audio.duration * (percentage / 100);
+	audioElement.setTime(seconds);
+}
 
 function prevSong() {
-	if(audioElement.audio.currentTime >= 3 || currentIndex == 0){
+	if(audioElement.audio.currentTime >= 3 || currentIndex == 0) {
 		audioElement.setTime(0);
 	}
-	else{
+	else {
 		currentIndex = currentIndex - 1;
 		setTrack(currentPlaylist[currentIndex], currentPlaylist, true);
 	}
@@ -77,22 +90,24 @@ function prevSong() {
 
 function nextSong() {
 
-	if(repeat == true){
+	if(repeat == true) {
 		audioElement.setTime(0);
 		playSong();
 		return;
 	}
+
 	if(currentIndex == currentPlaylist.length - 1) {
 		currentIndex = 0;
-	}else{
+	}
+	else {
 		currentIndex++;
 	}
 
-	var trackToPlay = currentPlaylist[currentIndex];
+	var trackToPlay = shuffle ? shufflePlaylist[currentIndex] : currentPlaylist[currentIndex];
 	setTrack(trackToPlay, currentPlaylist, true);
 }
 
-function setRepeat(){
+function setRepeat() {
 	repeat = !repeat;
 	var imageName = repeat ? "repeat-active.png" : "repeat.png";
 	$(".controlButton.repeat img").attr("src", "assets/images/icons/" + imageName);
@@ -108,15 +123,49 @@ function setShuffle() {
 	shuffle = !shuffle;
 	var imageName = shuffle ? "shuffle-active.png" : "shuffle.png";
 	$(".controlButton.shuffle img").attr("src", "assets/images/icons/" + imageName);
+
+	if(shuffle == true) {
+		//Randomize playlist
+		shuffleArray(shufflePlaylist);
+		currentIndex = shufflePlaylist.indexOf(audioElement.currentlyPlaying.id);
+	}
+	else {
+		//shuffle has been deactivated
+		//go back to regular playlist
+		currentIndex = currentPlaylist.indexOf(audioElement.currentlyPlaying.id);
+	}
+
 }
+
+function shuffleArray(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
 
 function setTrack(trackId, newPlaylist, play) {
 
-	currentIndex = currentPlaylist.indexOf(trackId);
+	if(newPlaylist != currentPlaylist) {
+		currentPlaylist = newPlaylist;
+		shufflePlaylist = currentPlaylist.slice();
+		shuffleArray(shufflePlaylist);
+	}
+
+	if(shuffle == true) {
+		currentIndex = shufflePlaylist.indexOf(trackId);
+	}
+	else {
+		currentIndex = currentPlaylist.indexOf(trackId);
+	}
 	pauseSong();
 
 	$.post("includes/handlers/ajax/getSongJson.php", { songId: trackId }, function(data) {
-	
+
 		var track = JSON.parse(data);
 		$(".trackName span").text(track.title);
 

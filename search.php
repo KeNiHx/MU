b0e8cae2a07f57ebb7f6889b575a078aa0e9001b
KeNiHx <klam291@mtroyal.ca>
2019-12-13 18:48:03 -0700

    added focus to search bar

--- search.php
+++ search.php
@@ -9,6 +9,30 @@ if(isset($_GET['term'])){
 ?>
 
 <div class="searchContainer">
-    <h4>Search for an artist, album or song</h4>
-    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..."></input>
+
+	<h4>Search for an artist, album or song</h4>
+	<input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="var val=this.value; this.value=''; this.value= val;">
+
 </div>
+
+<script>
+
+$(".searchInput").focus();
+
+$(function() {
+	var timer;
+
+	$(".searchInput").keyup(function() {
+		clearTimeout(timer);
+
+		timer = setTimeout(function() {
+			var val = $(".searchInput").val();
+			openPage("search.php?term=" + val);
+		}, 2000);
+
+	})
+
+
+})
+
+</script>

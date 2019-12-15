5e71eaa75e6e8cc21d9ba87cc7011b9618e11513
KeNiHx <klam291@mtroyal.ca>
2019-12-15 11:10:13 -0700

    added the playlist class

--- yourMusic.php
+++ yourMusic.php
@@ -19,13 +19,14 @@ include("includes/includedFiles.php");
         }
 
 		while($row = mysqli_fetch_array($playlistQuery)) {
-	
+            
+            $playlist = new Playlist($con, $row);
             echo "<div class='gridViewItem'>
                         <div class='playlistImage'>
                             <img src='assets/images/icons/playlist.png'>
                         </div>
                         <div class='gridViewInfo'>"
-                            . $row['name'] .
+                            . $playlist->getName() .
                          "</div>
 
 				</div>";
